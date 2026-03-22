4d589c668ea007ced5dabf812b31ecd68b5979b3
ayush git <itstiwarianshu357@gmail.com>
2026-03-23 00:44:18 +0530

    utilitis are created for code reusability

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB=async()=>{
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n DB is connected Succesfully!! On port ${connectionInstance.connection.host}`)
+        console.log(`\n DB is connected Succesfully!! On port: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log(error);
         process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app }from "./app.js";
 
 dotenv.config({
     path:"./env"
@@ -8,3 +9,11 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("DB connection failed:", err)
+})
